704ea864b747337c03c1ff83c72ce9a62325e1ec
Athlan <athlan@vgroup.pl>
2014-05-04 20:09:17 +0200

    Process all image

--- test.m
+++ test.m
@@ -1,12 +1,13 @@
-P = zeros(180, 65);
-%P = rand([180, 65]);
-for i=1:65
+range = 3;
+P = zeros(180, 65); % here image shoule be loaded
+
+for i=1:65 %test data
     P(1, i) = i;
 end
 
-rowNumber = 1;
-range = 3;
+P_rows = size(P, 1);
 
-A = P(rowNumber, :);
-FBUF = FiltracjaRzutow(P(rowNumber, :), range);
-%P(rowNumber, :) = FBUF;
+for rowNumber = 1:P_rows,
+    FBUF = FiltracjaRzutow(P(rowNumber, :), range);
+    P(rowNumber, :) = FBUF;
+end;
